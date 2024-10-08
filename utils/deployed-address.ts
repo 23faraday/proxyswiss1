a6ce213731f676a85c8cf9edbab3b193aa231944
Nemo Obias <23faraday@gmail.com>
2024-10-08 23:10:46 +0000

    initial commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xe5888CEB867D231B576F96f82Bc5046f506f3034'
+export const ProxyAdmin = '0x74B5Aa2171ff691cC0211aAdeCeCa2B17Baa48F2'
+export const SWTRImplementation = '0xB9C0f44D8bE1827cc1B0D5BD21C9e971dc3A6b66'
+    
